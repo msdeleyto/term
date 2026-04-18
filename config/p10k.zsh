b822abd5ad1e88c822af0d176ecef5ea90662075
msdeleyto <msd117c@gmail.com>
2026-04-18 18:05:34 +0200

    fix aliases not working

--- config/p10k.zsh
+++ config/p10k.zsh
@@ -144,9 +144,8 @@
     # ── Hot-reload ────────────────────────────────────────────────────────────
     typeset -g POWERLEVEL9K_DISABLE_HOT_RELOAD=true
 
-    # Apply config.
-    (( ${#p10k_config_opts} )) && setopt ${p10k_config_opts[@]}
   }
 } always {
+  (( ${#p10k_config_opts} )) && setopt ${p10k_config_opts[@]}
   'builtin' 'unset' 'p10k_config_opts'
 }
